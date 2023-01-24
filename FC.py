1383ca84e242c4d8f585ac8d5dec08df299bc674
gacloudi <gacloudi7@gmail.com>
2023-01-24 19:08:19 +0530

    b

--- FC.py
+++ FC.py
@@ -284,11 +284,10 @@ st.markdown("""
 #MainMenu {visibility: hidden;}
 footer {visibility: hidden;}
 header {visibility: hidden;}
+body {
 background-image: url("https://cdn.pixabay.com/photo/2019/04/24/11/27/flowers-4151900_960_720.jpg");
 background-attachment: fixed;
 background-size: cover
-         
-     )
-
+}
 </style>
 """, unsafe_allow_html=True)
